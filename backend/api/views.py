3a95f70d1b6321dcb47a24befe2c644fcdaafa4c
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-27 23:51:22 +0330

    create view for all services

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+from .serializers import ServiceSerializer
+from .models import Service
 # Create your views here.
+
+
+@api_view(['GET'])
+def all_services(request):
+    if request.method == 'GET':
+        services = Service.objects.all()
+        serializer = ServiceSerializer(services,many=True)
+        return Response(serializer,status=status.HTTP_200_OK)
