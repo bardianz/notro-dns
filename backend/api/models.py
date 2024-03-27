91b26ab692dfad3dc0812f6c0c46e70e3ba91edd
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-27 23:43:34 +0330

    create service model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=200)
+    prefred_ip = models.CharField(max_length=15)
+    alternate_ip = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.name
