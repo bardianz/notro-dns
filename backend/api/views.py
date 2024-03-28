645d38697192d16dcc682874e4e46bebc0f97d11
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-28 16:20:12 +0330

    create index page

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +8,19 @@ from rest_framework import status
 from .serializers import ServiceSerializer
 from .models import Service
 # Create your views here.
+
+
+def index_page(request):
+    try:
+        services = Service.objects.all()
+    except Service.DoesNotExist:
+        raise Http404("Service not exist")
+
+    return render(request, "api/index.html", {"services": services})
+
+
+
+
 
 
 @api_view(['GET'])
