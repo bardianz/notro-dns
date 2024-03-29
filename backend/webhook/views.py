3eb7671e619d73fcef1532ef93b03c22efc2c408
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 23:56:46 +0330

    another change......

--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -2,22 +2,19 @@ import subprocess
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import os
 
 
 
 @api_view(['POST'])
 def webhook_git_pull(request):
     if request.method == 'POST':
-        # subprocess.run(['cd', 'dnschanger/dns-changer'])
-
-
-        
-
         directory = 'dns-changer'
         try:
             # اجرای دستور cd در یک فرآیند فرعی
-            subprocess.run(["cd", directory], shell=True, check=True)
-        except subprocess.CalledProcessError as e:
+            # subprocess.run(["cd", directory], shell=True, check=True)
+            os.chdir(directory)
+        except Exception as e:
             print(f"Error {e}")
             return Response({"error": e},status=status.HTTP_406_NOT_ACCEPTABLE)
         
@@ -28,10 +25,42 @@ def webhook_git_pull(request):
             return Response({"error": e , "pwd" : pwd_output}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
-        subprocess.run(['git', 'pull'])
+        # subprocess.run(['git', 'pull'])
+        os.system("git pull")
+
         return Response({"pwd" : pwd_output},status=status.HTTP_200_OK)
     else:
         return Response(None,status=status.HTTP_400_BAD_REQUEST,)
+    
+
+
+# @api_view(['POST'])
+# def webhook_git_pull(request):
+#     if request.method == 'POST':
+#         # subprocess.run(['cd', 'dnschanger/dns-changer'])
+
+
+        
+
+#         directory = 'dns-changer'
+#         try:
+#             # اجرای دستور cd در یک فرآیند فرعی
+#             subprocess.run(["cd", directory], shell=True, check=True)
+#         except subprocess.CalledProcessError as e:
+#             print(f"Error {e}")
+#             return Response({"error": e},status=status.HTTP_406_NOT_ACCEPTABLE)
+        
+#         try:
+#             result = subprocess.run(["pwd"], shell=True, check=True, stdout=subprocess.PIPE, text=True)
+#             pwd_output = result.stdout.strip()
+#         except subprocess.CalledProcessError as e:
+#             return Response({"error": e , "pwd" : pwd_output}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+#         subprocess.run(['git', 'pull'])
+#         return Response({"pwd" : pwd_output},status=status.HTTP_200_OK)
+#     else:
+#         return Response(None,status=status.HTTP_400_BAD_REQUEST,)
     
 
 @api_view(['POST'])
