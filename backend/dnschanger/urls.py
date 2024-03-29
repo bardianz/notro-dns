e6216e7e109a8eeab43e4b31eb01942c3aaeef3a
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 13:27:57 +0330

    add static url to urls.py

--- backend/dnschanger/urls.py
+++ backend/dnschanger/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from api.views import index_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', index_page),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
