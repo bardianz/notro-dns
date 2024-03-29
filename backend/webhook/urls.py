941e81b203653e0e62e6ae7c192efd094bc45716
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 23:15:24 +0330

    change webhook

--- backend/webhook/urls.py
+++ backend/webhook/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import webhook_handler
+from .views import webhook_git_pull,webhook_reloader
 
 urlpatterns= [
-    path("",webhook_handler)
+    path("pull/",webhook_git_pull),
+    path("reload/",webhook_reloader),
 ]
--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -6,17 +6,18 @@ from rest_framework import status
 
 
 @api_view(['POST'])
-def webhook_handler(request):
+def webhook_git_pull(request):
     if request.method == 'POST':
-        # subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
-        # print("after cd --------------------------------------")
         subprocess.run(['git', 'pull'])
-        # subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
-        # print("before reload --------------------------------------")
+        return Response(None,status=status.HTTP_200_OK)
+    else:
+        return Response(None,status=status.HTTP_400_BAD_REQUEST)
+    
 
+@api_view(['POST'])
+def webhook_reloader(request):
+    if request.method == 'POST':
         subprocess.run(['touch', '/var/www/your_domain_wsgi.py'])
-        # print("after reload --------------------------------------")
-
         return Response(None,status=status.HTTP_200_OK)
     else:
         return Response(None,status=status.HTTP_400_BAD_REQUEST)
