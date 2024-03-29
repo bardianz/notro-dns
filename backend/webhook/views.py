16dbafe7e271397534b2c94761124b9d02690737
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 23:10:42 +0330

    change webhook

--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -8,14 +8,14 @@ from rest_framework import status
 @api_view(['POST'])
 def webhook_handler(request):
     if request.method == 'POST':
-        subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
-        print("after cd --------------------------------------")
+        # subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
+        # print("after cd --------------------------------------")
         subprocess.run(['git', 'pull'])
-        subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
-        print("before reload --------------------------------------")
+        # subprocess.run(['cd', '/home/dnschanger/dns-changer/backend'])
+        # print("before reload --------------------------------------")
 
         subprocess.run(['touch', '/var/www/your_domain_wsgi.py'])
-        print("after reload --------------------------------------")
+        # print("after reload --------------------------------------")
 
         return Response(None,status=status.HTTP_200_OK)
     else:
