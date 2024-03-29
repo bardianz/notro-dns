a1180fcb8438d3defec3c421250c2393dab449f3
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 23:28:11 +0330

    change webhook

--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 @api_view(['POST'])
 def webhook_git_pull(request):
     if request.method == 'POST':
+        subprocess.run(['cd', 'dnschanger/dns-changer'])
         subprocess.run(['git', 'pull'])
         return Response(None,status=status.HTTP_200_OK)
     else:
