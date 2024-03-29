daa4a28cf358685c47f70bfd6d4a61a538c1e9dc
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 22:12:17 +0330

    change webhook view

--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -1,24 +1,14 @@
-from django.shortcuts import render
+import subprocess
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-# Create your views here.
-
-import subprocess
-from django.http import HttpResponse, HttpResponseBadRequest
-# def webhook_handler(request):
-#     if request.method == 'POST':
-#         # Pull changes from Git repository
-#         subprocess.run(['git', 'pull'])
-#         return HttpResponse('Webhook received and changes pulled successfully.')
-#     else:
-#         return HttpResponseBadRequest('Invalid request method.')
 
 @api_view(['POST'])
 def webhook_handler(request):
     if request.method == 'POST':
         subprocess.run(['git', 'pull'])
+        subprocess.run(['touch', ' /var/www/your_domain_wsgi.py'])
         return Response(None,status=status.HTTP_200_OK)
     else:
         return Response(None,status=status.HTTP_400_BAD_REQUEST)
