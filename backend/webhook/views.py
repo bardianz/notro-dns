fa14a1b5f6c9f1fb35c85fd97fd40787531cdbaa
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-29 22:46:46 +0330

    edit webhook

--- backend/webhook/views.py
+++ backend/webhook/views.py
@@ -2,13 +2,16 @@ import subprocess
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.management import call_command
+
 
 
 @api_view(['POST'])
 def webhook_handler(request):
     if request.method == 'POST':
         subprocess.run(['git', 'pull'])
-        subprocess.run(['touch', '/var/www/your_domain_wsgi.py'])
+        # subprocess.run(['touch', '/var/www/your_domain_wsgi.py'])
+        call_command('runserver', '--noreload')
         return Response(None,status=status.HTTP_200_OK)
     else:
         return Response(None,status=status.HTTP_400_BAD_REQUEST)
