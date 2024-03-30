763618075365e900259426329f1544992b23f11f
Bardia Nikbakhsh <bardianikbakhsh@gmail.com>
2024-03-30 12:56:21 +0330

    register helppage to admin

--- backend/website/admin.py
+++ backend/website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from django.http import HttpRequest
+from website.models import HelpPage
 # Register your models here.
+
+class WebsiteAdmin(admin.ModelAdmin):
+    # def has_add_permission(self, request: HttpRequest) -> bool:
+    #     return False
+    pass
+
+admin.site.register(HelpPage,WebsiteAdmin)
